36fc31ffb61df5cc277c0137078d2aa299f4b414
NatanaelBT <natanaelb1403@gmail.com>
2020-06-03 21:10:47 -0300

    o nextLevelBooster me quebrou. Nesse pode ter erros

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,44 +1,100 @@
-function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]");
-    
+function ufs() {
+    const ufselect = document.querySelector("select[name=uf]")
+
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res => res.json() )
+
+    .then( res => res.json() )
     .then( states => {
 
-        for( const state of states){
-            ufSelect.innerHTML +=  `<option value="${state.id}"> ${state.nome} </option>`
+        for( state of states ) {
+            ufselect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
         }
-
-        
-    });
+       
+    } )
 }
 
-populateUFs();
+ufs()
 
-function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]");
-    const stateInput = document.querySelector("input[name=state]");
+function getcities(event) {
+    const cityselect = document.querySelector("select[name=city]")
 
-    const ufValue = event.target.value
-
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
-
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    const stateinput = document.querySelector("input[name=state]")
     
-    fetch(url)
-    .then(res => res.json() )
-    .then( cities => {
+    const ufvalue = event.target.value
 
-        for( const city of cities){
-            citySelect.innerHTML +=  `<option value="${city.id}"> ${city.nome} </option>`
+    const indexOfSelectedState = event.target.selectedIndex 
+    stateinput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufvalue}/municipios`
+
+    cityselect.innerHTML = "<option value>Selecione a Cidade</option>"
+    cityselect.disabled = true
+
+        fetch(url)
+        .then( res => res.json() )
+        .then( cities => {
+            
+
+        for( const city of cities ) {
+            cityselect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
-        citySelect.disabled = false;
-    });
+        cityselect.disabled = false
+
+    } )
 }
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+    .addEventListener("change", getcities)
+
+
+    // itens de coleta
+    const itemsToCollect = document.querySelectorAll(".items-grid li")
     
+    for (const item of itemsToCollect) {
+        item.addEventListener("click", handleSelectedItem)
+    }
+
+    const collectedItems = document.querySelector("input[name=items]")
+
+    let selectedItems = []
+
+    function handleSelectedItem(event) {
+        const itemLi = event.target
+
+        //adicionar ou remover uma class com js
+        itemLi.classList.toggle("selected")
+        
+
+        const itemId = eventLi.dataset.id
+
+        
+
+        //verificar se existem itens selecionados
+        //se sim, pegar os itens selecionados
+
+        const alreadySelected = selectedItems.findIndex( item => {
+            const itemFound = item === itemId //isso será true ou false
+            return itemFound
+        })
+
+        //se já estiver selecionado,
+        if(alreadySelected >= 0) {
+            //tirar da seleção
+            const filteredItems = selectedItems.filter( item => {
+                const itemsIsDifferent = item != itemId  //false
+                return itemsIsDifferent
+            })
+
+            selectedItems = filteredItems
+        } else {
+            //se não estiver selecionado, 
+            //adicionar a seleção
+            selectedItems.push(itemId)
+        }
+
+        
+        //atualizar o campo escondido com os itens selecionados
+        collectedItems.value = selectedItems
+    }
